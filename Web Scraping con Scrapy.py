07d69199742732553c800c96f4d998e02fcdc0b2
Frank Andrade <55821184+ifrankandrade@users.noreply.github.com>
2021-09-01 01:04:17 -0500

    Add files via upload

---
+++ Web Scraping con Scrapy.py
@@ -0,0 +1,24 @@
+import scrapy
+
+
+class WorldometersSpider(scrapy.Spider):
+    name = 'worldometers'
+    allowed_domains = ['www.worldometers.info/']
+    start_urls = ['https://www.worldometers.info/world-population/population-by-country/']
+
+    def parse(self, response):
+        # Obtener todas las filas (rows) de la tabla
+        rows = response.xpath('//tr')
+
+        for row in rows:
+            # Extraer el titulo (title), pais (country) y poblacion (population)
+            # title = response.xpath('//h1/text()').get()
+            countries = row.xpath('./td/a/text()').get()
+            population = row.xpath('./td[3]/text()').get()
+
+            # Devolver data extraida
+            yield {
+                # 'titles': title,
+                'countries': countries,
+                'population': population,
+            }
