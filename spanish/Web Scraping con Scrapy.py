cad185f46fbd53670608c91b01fda47b09e27ec6
Frank Andrade <55821184+ifrankandrade@users.noreply.github.com>
2021-09-04 23:55:59 -0500

    Update Web Scraping con Scrapy.py

--- spanish/Web Scraping con Scrapy.py
+++ spanish/Web Scraping con Scrapy.py
@@ -3,7 +3,7 @@ import scrapy
 
 class WorldometersSpider(scrapy.Spider):
     name = 'worldometers'
-    allowed_domains = ['www.worldometers.info/']
+    allowed_domains = ['www.worldometers.info']
     start_urls = ['https://www.worldometers.info/world-population/population-by-country/']
 
     def parse(self, response):
