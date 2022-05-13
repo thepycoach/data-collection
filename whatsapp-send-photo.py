ed362b0929c9e93ed0d3bc8956ea016c745692b7
Frank Andrade <55821184+ifrankandrade@users.noreply.github.com>
2022-05-12 20:26:17 -0500

    Update and rename 2.whatsapp-send-photo.py to whatsapp-send-photo.py

--- whatsapp-send-photo.py
+++ whatsapp-send-photo.py
@@ -5,7 +5,6 @@ import time
 path = ''  # Introduce your chromedriver path here
 website = "https://web.whatsapp.com/"
 phone_number = ""  # Write a phone number here
-message = "Test Photo"
 photo_path = ""  # introduce your photo path here
 
 service = Service(executable_path=path)
@@ -19,8 +18,7 @@ search_phone.send_keys(phone_number)
 time.sleep(2)
 
 contacts = driver.find_elements(by='xpath', value='//div[@aria-label="Search results."]//div[@data-testid="cell-frame-container"]')
-phone_contact = contacts[-1]  # picks last element in the list (this one represents first in the whatsapp search list)
-phone_contact.click()
+contacts[-1].click()  # picks last element in the list (this one represents first in the whatsapp search list)
 time.sleep(2)
 
 # upload and send photo
@@ -32,10 +30,6 @@ time.sleep(2)
 attach_image_input = driver.find_element(by='xpath', value='//input[@accept="image/*,video/mp4,video/3gpp,video/quicktime"]')
 attach_image_input.send_keys(photo_path)
 time.sleep(2)
-
-# send a message with photo
-text_with_image = driver.find_element(by='xpath', value='//div[@data-testid="drawer-middle"]//div[@role="textbox"]')
-text_with_image.send_keys(message)
 
 send_image_button = driver.find_element(by='xpath', value='//div[@data-testid="drawer-middle"]//span[@data-testid="send"]')
 send_image_button.click()
